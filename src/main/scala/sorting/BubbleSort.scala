c9929e82515daf74aeb19947112266c0463e6e5e
Akhtyam Sakaev <akhtyam.sakaev@gmail.com>
2019-05-09 23:58:53 +0300

    update bubble sort

--- src/main/scala/sorting/BubbleSort.scala
+++ src/main/scala/sorting/BubbleSort.scala
@@ -9,19 +9,20 @@ object BubbleSort {
 
   /**
    * Returns partially desc ordered list
+   * Large values are heavier and therefore sink to the head of list
    * Right if there was swaps, Left otherwise
    *
    * Example:
    * sweep(List(5, 1, 4, 2, 8)) == Right(List(8, 5, 2, 4, 1))
    */
   def sweep[A: Ordering](xs: List[A]): Either[List[A], List[A]] = {
-    @tailrec def sweepAux(s: S1[A]): S1[A] = s.l match {
-      case Nil                                 => s.copy(Nil, s.r)
-      case h :: Nil                            => s.copy(Nil, h :: s.r)
-      case a :: b :: t if Ordering[A].gt(a, b) => sweepAux(s.copy(a :: t, b :: s.r, modified = true))
-      case a :: b :: t                         => sweepAux(s.copy(b :: t, a :: s.r))
+    @tailrec def loop(s: S1[A]): S1[A] = s match {
+      case S1(Nil, _, _)                                 => s
+      case S1(h :: Nil, r, _)                            => s.copy(Nil, h :: r)
+      case S1(a :: b :: t, r, _) if Ordering[A].gt(a, b) => loop(s.copy(a :: t, b :: r, modified = true))
+      case S1(a :: b :: t, r, _)                         => loop(s.copy(b :: t, a :: r))
     }
-    val s = sweepAux(S1(xs, Nil, modified = false))
+    val s = loop(S1(xs, Nil, modified = false))
     if (s.modified) Right(s.r) else Left(s.r)
   }
 
@@ -32,16 +33,16 @@ object BubbleSort {
    * bubbleSort(List(5, 1, 4, 2, 8)) == List(1, 2, 4, 5, 8)
    */
   def bubbleSort[A: Ordering](xs: List[A]): List[A] = {
-    @tailrec def sortAux(s: S2[A]): S2[A] = s.l match {
-      case Nil      => S2(Nil, s.r)
-      case h :: Nil => S2(Nil, h :: s.r)
-      case ys =>
-        sweep(ys) match {
-          case Right(h :: t) => sortAux(S2(t, h :: s.r))
-          case _             => S2(Nil, ys ++ s.r)
+    @tailrec def loop(s: S2[A]): S2[A] = s match {
+      case S2(Nil, _)      => s
+      case S2(h :: Nil, r) => S2(Nil, h :: r)
+      case S2(l, r) =>
+        sweep(l) match {
+          case Right(h :: t) => loop(S2(t, h :: r))
+          case _             => S2(Nil, l ++ r)
         }
     }
-    sortAux(S2(xs, Nil)).r
+    loop(S2(xs, Nil)).r
   }
 
 }
