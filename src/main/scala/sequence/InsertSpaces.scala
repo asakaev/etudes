f759fed55d15c6cd21beb98ce3573db99e40aadf
Akhtiam Sakaev <akhtiam.sakaev@gmail.com>
2023-04-22 01:24:21 +0300

    Use Stream for InsertSpaces

--- src/main/scala/sequence/InsertSpaces.scala
+++ src/main/scala/sequence/InsertSpaces.scala
@@ -1,42 +1,29 @@
 package sequence
 
-import scala.annotation.tailrec
-
 object InsertSpaces {
 
   def insertSpaces(dict: Set[String], text: String): List[String] = {
-    def go(s: String, words: List[String], seen: Set[String]): List[List[String]] = {
-      if (s.isEmpty) List(words.reverse)
-      else {
-        val found: Option[(String, String)] =
-          chunks(s).find { case (w, _) => dict.contains(w) && !seen.contains(w) }
-
-        found match {
-          case None => Nil
-          case Some((word, rem)) =>
-            go(rem, word :: words, Set.empty) ++
-              go(word + rem, words, seen + word)
+    def go(s: String, words: List[String], seen: Set[String]): Stream[List[String]] =
+      if (s.isEmpty) Stream(words.reverse)
+      else
+        chunks(s).find { case (w, _) => dict.contains(w) && !seen.contains(w) } match {
+          case None           => Stream.empty
+          case Some((w, rem)) => go(rem, w :: words, Set.empty) ++ go(w + rem, words, seen + w)
         }
-      }
-    }
 
-    go(text, Nil, Set.empty).map(_.mkString(" "))
+    go(text, Nil, Set.empty).map(_.mkString(" ")).toList
   }
 
   /**
     * Scan string
     * Example: abcd => [("a", "bcd"), ("ab", "cd"), ("abc", "d"), ("abcd", "")]
     */
-  def chunks(s: String): List[(String, String)] = {
-    @tailrec def go(xs: List[(String, String)], s: String): List[(String, String)] =
-      if (s.isEmpty) xs
-      else
-        xs match {
-          case Nil          => go(List(s.head.toString -> s.tail), s.tail)
-          case (l, r) :: tl => go((l + s.head, r.tail) :: (l, r) :: tl, s.tail)
-        }
+  def chunks(s: String): Stream[(String, String)] = {
+    def go(l: String, r: String): Stream[(String, String)] =
+      if (r.isEmpty) Stream.empty
+      else Stream((l + r.head, r.tail)) ++ go(l + r.head, r.tail)
 
-    go(Nil, s).reverse
+    go("", s)
   }
 
 }
