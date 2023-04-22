d8199834398a476e868791d28c8d306aca7e4d6b
Akhtiam Sakaev <akhtiam.sakaev@gmail.com>
2023-04-22 12:23:59 +0300

    Add InsertSpaces docs

--- src/main/scala/sequence/InsertSpaces.scala
+++ src/main/scala/sequence/InsertSpaces.scala
@@ -2,6 +2,20 @@ package sequence
 
 object InsertSpaces {
 
+  /**
+    * Write a function that will add spaces to the input string to form valid words.
+    * For example, the string "thisisawesome" can have many solutions.
+    * insertSpaces("thisisawesome") would return:
+    * [
+    *  "this is a we so me",
+    *  "this is a we some",
+    *  "this is awe so me",
+    *  "this is awe some",
+    *  "this is awesome"
+    *  ]
+    *
+    *  val dict = Set("this", "is", "awesome", "a", "aw", "awe", "some", "so", "me", "i", "we")
+    */
   def insertSpaces(dict: Set[String], text: String): List[String] = {
     def go(s: String, words: List[String], seen: Set[String]): Stream[List[String]] =
       if (s.isEmpty) Stream(words.reverse)
