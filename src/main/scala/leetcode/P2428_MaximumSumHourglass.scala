52777b3bed916ef9b61c0415bf72c7906a77d7f3
Akhtiam Sakaev <akhtiam.sakaev@gmail.com>
2024-04-07 11:14:32 +0300

    Cleanup

--- src/main/scala/leetcode/P2428_MaximumSumHourglass.scala
+++ src/main/scala/leetcode/P2428_MaximumSumHourglass.scala
@@ -8,11 +8,11 @@ object P2428_MaximumSumHourglass {
         grid(i + 1)(j + 1) +
         grid(i + 2).slice(j, j + 3).sum
 
-    def go(i: Int, j: Int, n: Int): Int = {
+    def go(i: Int, j: Int, n: Int): Int =
       if (i > grid.size - 3) n
       else if (j > grid(0).size - 3) go(i + 1, 0, n)
       else go(i, j + 1, hourglass(grid, i, j).max(n))
-    }
+
     go(0, 0, 0)
   }
 
