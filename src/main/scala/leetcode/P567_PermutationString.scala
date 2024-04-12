ba7a958fa0a96220252bf2a5e2c2cac2b3a1f7c6
Akhtiam Sakaev <akhtiam.sakaev@gmail.com>
2024-04-12 12:48:58 +0300

    Cleanup 567

--- src/main/scala/leetcode/P567_PermutationString.scala
+++ src/main/scala/leetcode/P567_PermutationString.scala
@@ -6,24 +6,24 @@ object P567_PermutationString {
     def occurrences(s: String): Map[Char, Int] =
       s.groupMapReduce(identity)(_ => 1)(_ + _)
 
-    val m1: Map[Char, Int] = occurrences(s1)
+    val m0: Map[Char, Int] = occurrences(s1)
 
     def go(m: Map[Char, Int], i: Int): Boolean =
-      if (i >= s2.size - s1.size) m1 == m
-      else if (m1 == m) true
+      if (i >= s2.size - s1.size) m == m0
+      else if (m == m0) true
       else {
-        val m2: Map[Char, Int] =
+        val m1: Map[Char, Int] =
           m
             .updatedWith(s2(i)) {
               case None    => None
-              case Some(n) => Option.when(n - 1 > 0)(n - 1)
+              case Some(n) => Option.when(n > 1)(n - 1)
             }
             .updatedWith(s2(i + s1.size)) {
               case None    => Some(1)
               case Some(n) => Some(n + 1)
             }
 
-        go(m2, i + 1)
+        go(m1, i + 1)
       }
 
     go(occurrences(s2.take(s1.size)), 0)
